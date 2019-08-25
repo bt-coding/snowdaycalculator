b1a98a1ae24c3c43f2649e41d9391f3eda7f909d
Brian Hulbert <brian.hulbert@me.com>
2019-08-25 17:38:27 -0400

    testing commiting through github desktop

--- src/snowdaycalculator/Test.java
+++ src/snowdaycalculator/Test.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+// test
 /**
  * Servlet implementation class Test
  */
