a180440c51c3fa49366c2ef7237295414ccf31d6
Brian Hulbert <brian.hulbert@me.com>
2019-08-25 22:00:13 -0400

    Update Weather.java

--- src/snowdaycalculator/Weather.java
+++ src/snowdaycalculator/Weather.java
@@ -3,6 +3,12 @@ import java.net.*;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 public class Weather {
 	public Weather() {
 		
@@ -10,7 +16,7 @@ public class Weather {
 	public static void getWeather(int zipcode) {
 		try {
 			String key = "37fdd7c46ca515fc4b1a10c205022244\r\n";
-			
+			/*
 			URLConnection connection = new URL("https://api.openweathermap.org/data/2.5/forecast?zip="+zipcode+"&mode=xml&&APPID="+key).openConnection();
 		    connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 		    connection.connect();
@@ -25,6 +31,16 @@ public class Weather {
 		    
 		    String get = sb.toString();
 		    System.out.println(get);
+		    */
+		    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse("https://api.openweathermap.org/data/2.5/forecast?zip="+zipcode+"&mode=xml&&APPID="+key);
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("forecast");
+			for(int nn = 0; nn < nList.getLength(); nn++) {
+				Node node = nList.item(nn);
+				System.out.println(node.getNodeName());
+			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
